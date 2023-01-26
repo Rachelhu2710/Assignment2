0fd7187b720ffed9f54b3b03b786312da75c7e00
Rachelhu2710 <S10247778@connect.np.edu.sg>
2023-01-26 22:46:33 +0800

    hover change pic

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,3 +1,31 @@
+function hoverwhite()
+{
+  document.getElementById("item1").src = "images/PUMA-x-MARKET-Graphic-Tee-Men-white.webp";
+}
+function hoverblack()
+{
+  document.getElementById("item1").src = "images/PUMA-x-MARKET-Graphic-Tee-Men-black.webp";
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var slideIndex = 0;
 // showSlides();
 
@@ -14,21 +42,21 @@
 // slides[slideIndex - 1].style.display = "block";
 // setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   slides[slideIndex-1].style.display = "block"; 
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
 // let slideIndex = 0;
 // showSlides();
 
