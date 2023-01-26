b72492b381e89a6e63f360836626a7390b4475dc
Rachelhu2710 <S10247778@connect.np.edu.sg>
2023-01-27 00:10:21 +0800

    hover image update

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ This is a link to my design wireframe for [phone]() version [web]() version.
 ### Future Features 
 ## Technology used
 - JavaScript
+- Jquery
 - CSS
 - Google Fonts
 - XD
---
+++ js/hoverpic.js
@@ -0,0 +1,48 @@
+function hoveritem1c1()
+{
+  document.getElementById("item1").src = "images/PUMA-x-MARKET-Graphic-Tee-Men-white.webp";
+}
+function hoveritem1c2()
+{
+  document.getElementById("item1").src = "images/PUMA-x-MARKET-Graphic-Tee-Men-black.webp";
+}
+function hoveritem2c1()
+{
+  document.getElementById("item2").src = "images/Fandom-Shorts-Men-Black.webp";
+}
+function hoveritem2c2()
+{
+  document.getElementById("item2").src = "images/Fandom-Shorts-Men-Lavender.webp";
+}
+function hoveritem4c1()
+{
+  document.getElementById("item4").src = "images/Classics-Small-Logo-Tee-Men-blue.webp";
+}
+function hoveritem4c2()
+{
+  document.getElementById("item4").src = "images/Classics-Small-Logo-Tee-Men-black.webp";
+}
+function hoveritem5c1()
+{
+  document.getElementById("item5").src = "images/Logo-Short-Sleeve-Men's-Running-Tee-grape.webp";
+}
+function hoveritem5c2()
+{
+  document.getElementById("item5").src = "images/Logo-Short-Sleeve-Men's-Running-Tee-red.webp";
+}
+function hoveritem7c1()
+{
+  document.getElementById("item7").src = "images/Classics-Relaxed-Crewneck-Sweatshirt-Men-Blue.webp";
+}
+function hoveritem7c2()
+{
+  document.getElementById("item7").src = "images/Classics-Relaxed-Crewneck-Sweatshirt-Men-Green.webp";
+}
+function hoveritem8c1()
+{
+  document.getElementById("item8").src = "images/Classics-Relaxed-Hoodie-Men-grey.webp";
+}
+function hoveritem8c2()
+{
+  document.getElementById("item8").src = "images/Classics-Relaxed-Hoodie-Men-black.webp";
+}
