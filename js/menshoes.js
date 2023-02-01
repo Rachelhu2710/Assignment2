1d725ba6d77c82c68a784eb260342a8698329ab6
Rachelhu2710 <S10247778@connect.np.edu.sg>
2023-02-02 01:51:42 +0800

    men products

--- js/menshoes.js
+++ js/menshoes.js
@@ -1,10 +1,28 @@
-//how to capture id when click using jquery?
-$('div').on('click',function(ev){
-    ev.stopPropagation()
-    checkEl = $(this).attr('id');
-    localStorage.setItem('id',checkEl)
-});
+let links = 
+[
+  {idname: "item1-1",link:"images/NCT_Sneakers.webp",cat:"shoes"},
+  {idname: "item1-2",link:"images/Slipstream-Sneakers-black.webp",cat:"shoes"},
+  {idname: "item2-1",link:"images/Flyer_Flex_Running_Shoes.webp",cat:"shoes"},
+  {idname: "item2-2",link:"images/Flyer-Flex-Running-Shoes-black.webp"}
+]
+const buttons = document.getElementsByTagName("a");
+const buttonPressed = e => {  
+  localStorage.setItem("id",e.target.id)
+  localStorage.setItem("class",e.target.className)
+};
 
+for (let button of buttons) {
+  button.addEventListener("click", buttonPressed);
+}
 
-
+for(let name of shoelinks)
+{
+  if (localStorage.getItem("class")=== name.cat)
+  {
+    if (localStorage.getItem("id")=== name.idname)
+    {
+      document.querySelector("img").src = name.link  
+    }
+  }
+}
 
