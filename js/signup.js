39518ac644b026f8c9bd9eaa92528a6ba07812fa
Rachelhu2710 <S10247778@connect.np.edu.sg>
2023-02-07 23:18:49 +0800

    validation and remove kids section

--- js/signup.js
+++ js/signup.js
@@ -41,6 +41,53 @@ $(document).ready(function(){
         let country = $("#country").val();
         // let PhoneNumber = $("#PhoneNumber").val();
         let gender=$("#gender").attr("data-gender");  
+        let text = /^[a-zA-Z]+$/;
+        // validate email
+        if (email === "") //make sure that email is not left blank
+        {
+          alert("Email cannot be left blank");
+          return;
+        }
+        else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) //make sure that email have a @ and .com
+        {
+          alert("Invalid email format")
+          return;
+        }
+      
+        // Validate password
+        if (password ==='') //make sure that password is not left blank
+        {
+          alert("Password cannot be left blank")
+          return;
+        }
+        else if (password.length < 8) //make sure that password is 8 characters long
+        {
+          alert("Password must be at least 8 characters long");
+          return;
+        }
+        //validate name
+        if (name ==='') //make sure that is not left empty
+        {
+          alert("First and Last Name cannot be left black");
+          return;
+        }
+        else if (name.match(text)) //make sure that name only have letters
+        {
+          alert("First and Last Name should only have letters");
+          return;
+        }
+        //validate country to make sure that one option is selected
+        if (country ==='')
+        {
+          alert("Please Select a Country");
+          return;
+        }
+        //validate gender to make sure either one is selected
+        if (gender ==='')
+        {
+          alert("Please Select a Gender");
+          return;
+        }
     
         //[STEP 3]: get form values when user clicks on send
         //Adapted from restdb api
