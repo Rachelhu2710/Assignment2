693a06926c67299f126420af2ea5ca306692b560
Rachelhu2710 <S10247778@connect.np.edu.sg>
2023-02-08 00:45:57 +0800

    signup

--- js/getallproducts.js
+++ js/getallproducts.js
@@ -1,4 +1,3 @@
-
 // localStorage.clear()
 const APIKEY="63df8e643bc6b255ed0c46a6";
 
@@ -22,13 +21,13 @@ const APIKEY="63df8e643bc6b255ed0c46a6";
         
        if(response[i]["productcode"]=="ms")
        {
-        shoesarray.push(response[i])
+        // shoesarray.push(response[i])
         localStorage.setItem(`ms${response[i]["id"]}`,JSON.stringify(response[i]))
 
        }
        else if(response[i]["productcode"]=="ma")
        {
-        accesoriesarray.push(response[i])
+        // accesoriesarray.push(response[i])
         localStorage.setItem(`ma${response[i]["id"]}`,JSON.stringify(response[i]))
 
   
